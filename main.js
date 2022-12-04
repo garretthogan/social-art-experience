780b3f16322bfdb9a72ec3e763b44dc1a1442da6
Garrett Hogan <garretthogan@Garretts-MacBook-Pro.local>
2022-12-04 09:38:44 -0800

    updated rotation calc

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Vector2, Vector3 } from 'three';
+import { Euler, Matrix4, Quaternion, Vector2, Vector3 } from 'three';
 import DecalsExperience from './experiences/decals';
 import FPSExperience from './experiences/fps';
 import Player from './objects/Player';
@@ -36,11 +36,19 @@ export function startExperience(experience) {
 	// maybe move this somewhere
 	let x = 0;
 	let y = 0;
+	let extraX = 0;
+	let extraY = 0;
 	window.addEventListener('touchstart', (event) => {
 		const touches = Array.from(event.touches);
 		const touch = touches[0];
 		x = touch.clientX;
 		y = touch.clientY;
+
+		const extraTouch = touch[1];
+		if (extraTouch) {
+			extraX = extraTouch.clientX;
+			extraY = extraTouch.clientY;
+		}
 	});
 
 	const user = new Player();
@@ -66,13 +74,14 @@ export function startExperience(experience) {
 			const currentX = extraTouch.clientX;
 			const currentY = extraTouch.clientY;
 
-			const diffX = currentX - x;
-			const diffY = currentY - y;
-			const velocity = new Vector2(diffX, diffY);
-			velocity.normalize();
+			const diffX = currentX - extraX;
+			const diffY = currentY - extraY;
+			const angle = Math.atan2(diffY, diffX);
+			const mat4 = new Matrix4().makeRotationAxis(new Vector3(0, 1, 0), angle);
 
-			camera.rotation.y -= velocity.x / 30;
-			camera.rotation.x -= velocity.y / 30;
+			camera.rotation.setFromRotationMatrix(mat4);
+			// camera.rotation.y -= (velocity.y / 500) * getDeltaTime();
+			// camera.rotation.x -= diffY / 50000;
 		}
 	});
 
